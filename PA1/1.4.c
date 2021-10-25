3551fd71c8ba8b2bdd4cc753a770f39a7ed194e2
LetongWang <wanglt@shanghaitech.edu.cn>
2021-10-25 14:36:37 +0800

    remove the chunch size

--- PA1/1.4.c
+++ PA1/1.4.c
@@ -2,7 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define N 512
-#define CHUNKSIZE 16
 void MM_outer(int n_thread){
     omp_set_num_threads(n_thread);//set number of threads here
     int i, j, k;
@@ -18,12 +17,13 @@ void MM_outer(int n_thread){
             C[i][j] = j-i*2;
         }
     }
-    int chunk = CHUNKSIZE;
-    for (int t = 0; t<10; t++){
+    // t is the repeat times
+    for (int t = 0; t<11; t++){
         start = omp_get_wtime(); //start time measurement
-        #pragma omp parallel shared(A,B,C,chunk) private(i, j, k, sum)
+        // begin of matrix multiplication
+        #pragma omp parallel shared(A,B,C) private(i, j, k, sum)
         {
-            #pragma omp for schedule(static, chunk)
+            #pragma omp for 
             for (i = 0; i < N; i++) {
                 for (j = 0; j < N; j++) {
                     sum = 0;
@@ -36,8 +36,10 @@ void MM_outer(int n_thread){
         }
         
         end = omp_get_wtime(); //end time measurement
-        printf("Time of computation %d: %f seconds\n", t, end-start);
-        time_sum += end-start;
+        if (t > 0){
+            printf("Time of computation %d: %f seconds\n", t, end-start);
+            time_sum += end-start;
+        }
     }
     printf("Average time of computation: %f seconds\n", time_sum/10);
 }
@@ -57,28 +59,35 @@ void MM_middle(int n_thread){
             C[i][j] = j-i*2;
         }
     }
-    int chunk = CHUNKSIZE;
-    for (int t = 0; t<10; t++){
+    // t is the repeat times
+    for (int t = 0; t<11; t++){
         start = omp_get_wtime(); //start time measurement
+        // begin of Matrix Multiplication
         for (i = 0; i < N; i++) {
-            #pragma omp parallel for schedule(static, chunk) private(j, k, sum)
-            for (j = 0; j < N; j++) {
-                sum = 0;
-                for (k=0; k < N; k++) {
-                    sum += A[i][k]*B[k][j];
+            #pragma omp parallel shared(i,A,B,C) private(j, k, sum)
+            {
+                #pragma omp for
+                for (j = 0; j < N; j++) {
+                    sum = 0;
+                    for (k=0; k < N; k++) {
+                        sum += A[i][k]*B[k][j];
+                    }
+                    C[i][j] = sum;
                 }
-                C[i][j] = sum;
             }
+            
         }
         end = omp_get_wtime(); //end time measurement
-        printf("Time of computation %d: %f seconds\n", t, end-start);
-        time_sum += end-start;
+        if (t>0){
+            printf("Time of computation %d: %f seconds\n", t, end-start);
+            time_sum += end-start;
+        }
     }
     printf("Average time of computation: %f seconds\n", time_sum/10);
 }
 
 int main(int argc, char **argv) {
-    int threads[4] = {1,4,8,16};
+    int threads[] = {1,4,8,16};
     if (argc < 2) {
         printf("Usage: ./MM [0:outer parall mm; 1: middle parallel mm]");
         return 0;
